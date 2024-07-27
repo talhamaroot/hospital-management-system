3778200ac45e3a9376d91e2281817f002660727d
TK Codes <talhacodes1@gmail.com>
2024-07-27 15:16:39 +0500

    print ledger

--- app/Filament/admin/Resources/LedgerResource/Pages/ListLedgers.php
+++ app/Filament/admin/Resources/LedgerResource/Pages/ListLedgers.php
@@ -15,6 +15,35 @@ class ListLedgers extends ListRecords
     {
         return [
             Actions\CreateAction::make(),
+            Actions\Action::make("print")
+            ->url(function () {
+                $data = $this->tableFilters;
+                $account_type = $data['user']['account_type'] ? $data['user']['account_type']:"null" ;
+                $account_id = "null";
+                if ($account_type == 'patient') {
+                    $account_id = $data['user']["patient_id"];
+                }
+                if ($account_type == 'employee') {
+                    $account_id = $data['user']["employee_id"];
+                }
+                if ($account_type == 'doctor') {
+                    $account_id = $data['user']["doctor_id"];
+                }
+                if ($account_type == 'system') {
+                    $account_id = $data['user']["account"];
+                }
+                if ($account_type == 'ot attendant') {
+                    $account_id = $data['user']["ot_attendant_id"];
+                }
+                if ($account_type == 'anesthesiologist') {
+                    $account_id = $data['user']["anesthesiologist_id"];
+                }
+                $date_from = $data['user']["created_from"] ? $data['user']["created_from"] : "null";
+                $date_to = $data['user']["created_until"] ? $data['user']["created_until"]  : "null";
+            
+                return (url("/print_ledger/$account_type/$account_id/$date_from/$date_to" ));
+            })
+            ->openUrlInNewTab()
 
         ];
     }
